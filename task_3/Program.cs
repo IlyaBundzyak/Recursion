f2ac84d2af43e1cb375b863c3e1f460e40fc8a4c
ilya bundzyak <bundzyak.91@mail.ru>
2023-12-27 23:44:21 +0300

    Решение задачи

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+Использовать рекурсию, не использовать циклы.*/
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 3, 9, 5, 32, -7, 68 };
+
+        PrintArrayFromEnd(array, array.Length - 1);
+
+
+        static void PrintArrayFromEnd(int[] arr, int i)
+        {
+            if (i >= 0)
+            {
+                Console.WriteLine(arr[i]);
+                PrintArrayFromEnd(arr, i - 1);
+            }
+        }
+    }
+
+}
