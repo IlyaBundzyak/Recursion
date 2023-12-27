4092379e380c60bf8f7ed206fdeb3c0fe67580f3
ilya bundzyak <bundzyak.91@mail.ru>
2023-12-27 23:05:52 +0300

    Решение задачи

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 1: Задайте значения M и N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы. */
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        PrintNaturalNumbers(M, N);
+    }
+
+    static void PrintNaturalNumbers(int start, int end)
+    {
+        if (start <= end)
+        {
+            Console.WriteLine(start);
+            PrintNaturalNumbers(start + 1, end);
+        }
+    }
+}
+
